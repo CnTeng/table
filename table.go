8ae7505fa2b1b082644c0e271f17379189f83a76
CnTeng <rxsnakepi@gmail.com>
2025-08-16 16:45:04 +0800

    docs: add example

--- table.go
+++ table.go
@@ -1,5 +1,30 @@
 // Package table offers an easy way to generate tables similar to Taskwarrior,
 // featuring 256-color support, automatic text wrapping and Markdown rendering.
+//
+//	tbl := table.NewTableWithStyle(&table.TableStyle{
+//		DefaultWidth:  80,
+//		FitToTerminal: false,
+//		WrapText:      true,
+//		Markdown:      true,
+//		HideEmpty:     true,
+//		OuterPadding:  0,
+//		InnerPadding:  1,
+//	})
+//
+//	bgGreen := text.BgGreen.Sprint("BgGreen")
+//	bgBlue := text.BgBlue.Sprint("BgBlue")
+//	fgRed := text.FgRed.Sprint("FgRed")
+//
+//	tbl.AddHeader("Feature", "Example", "Example")
+//	tbl.AddRow(table.Row{"**Color-256**", bgGreen + " " + bgBlue, fgRed})
+//	tbl.AddRow(table.Row{"**Wrap Text**", strings.Repeat("Hello World! ", 20), "Hello World!"})
+//	tbl.AddRow(table.Row{"Markdown", "[link](http://example.com)", "**Bold** _Italic_ `inline Code`"})
+//
+//	tbl.SetHeaderStyle(&table.CellStyle{
+//		CellAttrs: text.Colors{text.FgGreen, text.Underline},
+//	})
+//
+//	fmt.Print(tbl.Render())
 package table
 
 import (
