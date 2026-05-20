e887cd5c013508c2458cc897fe0b722d93e1ba6a
CnTeng <rxsnakepi@gmail.com>
2026-05-20 23:29:22 +0800

    bump: flake inputs

--- flake.nix
+++ flake.nix
@@ -1,55 +1,40 @@
 {
   description = "Simple table for go.";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    flake-parts.url = "github:hercules-ci/flake-parts";
-
-    git-hooks-nix = {
-      url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-
-      imports = [ inputs.git-hooks-nix.flakeModule ];
-
-      perSystem =
-        { config, pkgs, ... }:
-        {
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              go
-              gotools
-            ];
-            shellHook = config.pre-commit.installationScript;
-          };
-
-          formatter = pkgs.nixfmt-tree.override {
-            settings.formatter.gofumpt = {
-              command = "gofumpt";
-              excludes = [ "vendor/*" ];
-              includes = [ "*.go" ];
-              options = [ "-w" ];
-            };
-            runtimeInputs = [ pkgs.gofumpt ];
-          };
-
-          pre-commit.settings.hooks = {
-            treefmt = {
-              enable = true;
-              package = config.formatter;
-            };
-            commitizen.enable = true;
-          };
+    { nixpkgs, ... }:
+    let
+      forEachSystem =
+        f:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forEachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gotools
+          ];
+          CGO_ENABLED = "0";
         };
+      });
+
+      formatter = forEachSystem (
+        pkgs:
+        pkgs.nixfmt-tree.override {
+          settings.formatter.gofumpt = {
+            command = "gofumpt";
+            options = [ "-w" ];
+            includes = [ "*.go" ];
+            excludes = [ "vendor/*" ];
+          };
+          runtimeInputs = [ pkgs.gofumpt ];
+        }
+      );
     };
 }
